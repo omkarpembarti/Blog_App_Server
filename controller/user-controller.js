ccf7b46175508697ff6652e62580edd1cfe5338d
Omkar Pembarti <omkarpembarti@gmail.com>
2023-08-24 13:03:08 +0530

    Installed & integrated  bcrypt library to hash passwords

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -2,10 +2,15 @@
 //const User = require('./database/models/user.js');
 
 import User from "../database/models/user.js";
-
+import bcrypt from 'bcrypt'
 const registerUser = async (req, res) => {
     try {
-        const userData = req.body;
+
+        const hashpassword = await bcrypt.hash(req.body.password, 10);
+        const userData = {
+            userName: req.body.userName,
+            password: hashpassword
+        };
         const newUser = new User(userData);
         await newUser.save();
 
--- database/db.js
+++ database/db.js
@@ -5,9 +5,6 @@ const connection = async (userName, password) => {
     try {
         //const URI = `mongodb+srv://${userName}:${password}@tenet.mbed4r8.mongodb.net/sample_analytics?retryWrites=true&w=majority`;
         const URI = `mongodb+srv://${userName}:${password}@tenet.mbed4r8.mongodb.net/?retryWrites=true&w=majority`;
-
-
-
         await connect(URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
