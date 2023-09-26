efd6bf71d362b9a94cef955d9888a10d9c616d2c
Omkar Pembarti <omkarpembarti@gmail.com>
2023-09-26 10:45:04 +0530

    Fixed minor issues

--- controller/jwt-controller.js
+++ controller/jwt-controller.js
@@ -5,16 +5,16 @@ dotenv.config();
 export const authToken = (req, res, next) => {
 
     try {
-        //console.log('req.headers', req.headers);
+
         const authHeader = req.headers['authorization'];
-        //console.log('authHeader', authHeader);
+
         const token = authHeader.split(' ')[1];
         if (token == null)
             return res.status(401).json({ 'msg': 'Access token missing' });
 
         jwt.verify(token, process.env.ACCESS_SECRET_KEY, (error, user) => {
             if (error)
-                return res.status(403).json({ msg: 'Invalid Token' });
+                return res.status(403).json({ msg: 'Invalid Token-Session Expired' });
             //403 --> Understands the request, but refuses to authorize it
             req.user = user;
             next();
